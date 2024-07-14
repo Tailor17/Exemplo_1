9c7719c2ab489761519c417da2b0ca8c607d79bd
Tailor17 <tailorzitzke.pl013@academico.ifsul.edu.br>
2024-07-14 11:48:29 -0300

    Atualização
    Foi realizada a inserção do opção de limpar o banco de dados.

--- imports/api/threads.js
+++ imports/api/threads.js
@@ -1,16 +1,26 @@
-import {Meteor} from "meteor/meteor";
-import {Mongo} from "meteor/mongo";
-import {check} from "meteor/check";
+import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
+import { check } from 'meteor/check';
 
+// Defina sua coleção
 export const ThreadsCollection = new Mongo.Collection('threads');
 
-async function insertThread(text){
-    check(text, String);
-    
-    await ThreadsCollection.insertAsync({
-        text,
-        date: new Date()
-    });
+async function insertThread(text) {
+  check(text, String);
+
+  await ThreadsCollection.insertAsync({
+    text,
+    date: new Date()
+  });
 }
 
-Meteor.methods({insertThread});
+async function clearThreads() {
+  if (Meteor.isServer) {
+    await ThreadsCollection.remove({});
+  }
+}
+
+Meteor.methods({
+  insertThread,
+  clearThreads,
+});
